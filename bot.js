615ff5c5cb288d2efb4864e26f5d52efbce7bd54
Bill-1991 <altzebill@yahoo.com>
2024-01-29 21:58:06 +0200

    create documentation of scheduler

--- bot.js
+++ bot.js
@@ -22,50 +22,6 @@ function readyBot() {
 function userMessage(msg) {
 	if (msg.channel.id === process.env.CHANNEL_KEY)
 	{
-		let query = 'SELECT * FROM users;'
-		//let data = []
-		let result = []
-	
-		/*if (msg.content === 'first')
-		{
-			function getData(){
-				return new Promise(resolve=>{
-					database.db.all(query,[],(err,rows)=>{
-						if(err){
-							return console.error(err.message);
-						}
-						rows.forEach((row)=>{
-							data.push([row.name, row.day]);
-						});
-			
-						resolve(data);
-					});
-				});
-			}
-
-			async function waitForData() {
-				result = await getData()
-				let log = ''
-				for (let i = 0; i < result.length; i++)
-				{
-					log += `|${result[i][0]}| |${result[i][1]}|\n`
-				}
-				msg.reply(log)
-			} 
-
-			waitForData()
-		}*/
-		if (msg.content === 'second')
-		{
-			let name = 'third'
-			let result  = functions.showNameTable(query, name, database)
-			console.log(result)
-		}
-		//"CREATE TABLE users(name TEXT NOT NULL, day TEXT NOT NULL);";
-        //database.db.run(sql);
-	    //"INSERT INTO users VALUES ('first', 'false'),('second', 'true'),('third', 'true');";
-        //database.db.run(sql);
-        //let sql = "DROP TABLE users;";
-        //database.db.run(sql);
+		
 	}
 };
--- functions.js
+++ functions.js
@@ -1,15 +1,1 @@
 const database = require('./database.js')
-//function logResultOfQuery(result) {
-
-//}
-
-function showFullTable(result, query, database) {
-    query = 'SELECT * FROM users;'
-}
-
-function showNameTable(query, name, database) {
-    query = `SELECT * FROM users WHERE name=${name};`
-}
-
-
-module.exports = { showFullTable, showNameTable }
