ab2bef24117d5e9a5be6d8cc329bc097e9a21074
Andrey Dmitriev <admit8490@gmail.com>
2024-02-03 04:36:16 +0200

    command's deployment script

--- bot.js
+++ bot.js
@@ -14,8 +14,7 @@ client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	console.log(filePath);
+	const filePath = path.join(commandsPath, file);	
 	const command = require(filePath);
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
