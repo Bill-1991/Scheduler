0d45912c8bc5d8b5d72903e1602e7c851a656398
Andrey Dmitriev <admit8490@gmail.com>
2024-02-03 04:18:25 +0200

    retrieving commands

--- bot.js
+++ bot.js
@@ -1,7 +1,28 @@
-const { Client, GatewayIntentBits, Events } = require('discord.js');
+// discordJS imports
+const { Client, GatewayIntentBits, Events, Collection } = require('discord.js');
+// nodeJS imports
+const fs = require("node:fs");
+const path = require("node:path");
+// other imports
 require('dotenv').config();
 
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+//retrieving commands
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	console.log(filePath);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
